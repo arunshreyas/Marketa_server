8a47d792f83255e0b52615c471ad56c47e42c315
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2025-10-30 23:08:15 +0500

    cors policy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,13 +4,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
-<<<<<<< HEAD
     "https://marketa-ten.vercel.app",
     "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--cf284e50.local-credentialless.webcontainer-api.io",
-=======
-    "https://marketa-ten.vercel.app/",
-    "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--cf284e50.local-credentialless.webcontainer-api.io/",
->>>>>>> e795fda163cfd9083d52c3521ba6fd93fa4f80d2
     // Common hosted app domains (wildcards via RegExp)
     /https?:\/\/.*\.vercel\.app$/,
     /https?:\/\/.*\.netlify\.app$/,
