e795fda163cfd9083d52c3521ba6fd93fa4f80d2
Arun Shreyas Bhattiprolu <133573446+arunshreyas@users.noreply.github.com>
2025-10-30 21:52:34 +0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "https://marketa-ten.vercel.app/",
-    "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--cf284e50.local-credentialless.webcontainer-api.io/"
+    "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--3000--cf284e50.local-credentialless.webcontainer-api.io/",
     // Common hosted app domains (wildcards via RegExp)
     /https?:\/\/.*\.vercel\.app$/,
     /https?:\/\/.*\.netlify\.app$/,
